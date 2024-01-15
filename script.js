192138871ddfbbae3d3ccaabc9bed905d793a159
kap <kgdee7603@gmail.com>
2024-01-15 21:20:04 +0800

    commit

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const browserContent = document.querySelector(".browser-window .content")
 window.addEventListener('message', (event) => {
   const { image, backgroundColor, frameColor, textColor } = event.data
 
-  if (image) browserContent.style.backgroundImage = `url(${image})`
+  if (image) {
+    browserContent.style.backgroundImage = `url(${image})`
+    browserContent.querySelector(".logo").style.filter = 'brightness(0) invert(1)'
+  }
   if (backgroundColor) document.documentElement.style.setProperty("--background-color", backgroundColor)
   if (frameColor) document.documentElement.style.setProperty("--frame-color", frameColor)
   if (textColor) document.documentElement.style.setProperty("--text-color", textColor)
